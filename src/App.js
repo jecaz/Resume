ec0dccf3b7b98411266987f0e4302172bcad5273
Jelena <jelena.zobenica1@gmail.com>
2020-06-14 10:48:42 +0200

    Display json data in the About section

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header data={this.state.resumeData.main} />
-        <About />
+        <About data={this.state.resumeData.main} />
         <Resume />
         <Portfolio />
         <Testimonials />
